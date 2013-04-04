7d0dd3c867135f8ea988caee29214d24cd58f6b5
jfenwick <jfenwic1@unda.ecu>
2013-04-04 19:21:59 -0400

    Final additions/changes, not linted yet

--- public/javascripts/client-app.js
+++ public/javascripts/client-app.js
@@ -9,18 +9,16 @@ var main = function () {
     var resp = response.toString();
     for (var i = 0; i < happyWords.length; i++){
       if (happyWords[i]=== response.key){
-        $(".happy").append("<p>" + response.key + " : " + response.value + "</p>");
         happyCount = happyCount+parseInt(response.value);
       } else if(sadWords[i]===response.key) {
-        $(".sad").append("<p>" + response.key + " : " + response.value + "</p>");
-        sadCount = sadCount+parseInt(response.value);
+         sadCount = sadCount+parseInt(response.value);
       }
     }
   }
   $.getJSON("/counts.json", function (response) { 
     response.forEach(wordCount);
-    $("body").prepend("<p> Happy Count: " + happyCount + "</p>");
-    $("body").prepend("<p> Sad Count: " + sadCount + "</p>");
+    $('.happy').append("<p> Happy Count: " + happyCount + "</p>");
+    $('.sad').append("<p> Sad Count: " + sadCount + "</p>");
   });
 };
 
